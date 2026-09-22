043ff7c7f4bd2d9a9011b7b7f2afb0133baae8a4
Celio Sousa Silva <celiosousa1264@gmail.com>
2026-09-22 14:50:51 +0900

    importa template inklui halo autentikasaun ba login form

--- main/views.py
+++ main/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+
 
 # Create your views here.
+def dashboard(request):
+    return render(request, "admin_templates/index.html")
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('dashboard')
+        else:
+            messages.error(request, "Favor Prenxe Username no Password ho loos!")
+            return render(request, "admin_templates/auth-login.html")
+    return render(request, "admin_templates/auth-login.html")
+
+def logout_view(request):
+    logout(request, user)
+    return render (request, "admin_templates/auth-login.html")
--- website_apftl/settings.py
+++ website_apftl/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'main_front_end',
+    'main',
 ]
 
 MIDDLEWARE = [
